8917e0bb88b8ff9debb70d07756c947745f30956
Hemu2814196 <31968979+Hemu2814196@users.noreply.github.com>
2018-02-24 18:53:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req, res) {
+  res.send(path.join("article one requesteed and will be served"));
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));});
 });
+app.get('/article-two', function (req, res) {
+  res.send(path.join("article two requesteed and will be served"));});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+app.get('/article-three', function (req, res) {
+  res.send(path.join("article three requesteed and will be served"));
 });
 
 
